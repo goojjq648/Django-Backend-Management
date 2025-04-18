f71cd03dcf779ee299d25032693f8011c589530c
goojjq648 <goojjq648@gmail.com>
2025-04-18 11:50:05 +0800

    feat : google 登入的url

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views.restaurant_views import RestaurantViewSet
 from .views.search_views import SearchAddressView
+from .views.auth_views import google_callback
 
 router = DefaultRouter()
 router.register(r'restaurants', RestaurantViewSet)  # 讓 `/api/restaurants/` 成為 API 端點
@@ -9,4 +10,5 @@ router.register(r'restaurants', RestaurantViewSet)  # 讓 `/api/restaurants/` �
 urlpatterns = [
     path('', include(router.urls)),
     path('search/address/', SearchAddressView.as_view(), name='search_address'),  # /api/search/address/
+    path('google/callback', google_callback, name='google-login'),                # /api/google/callback
 ]
